d78a0938f917d0e31fdac0cc46d5af62ee1168ee
binthon <jakubmedyn@wp.pl>
2025-06-30 00:55:20 +0200

    update uwsgi

--- zadanie1/main.py
+++ zadanie1/main.py
@@ -3,7 +3,9 @@ import app.model
 
 app = createApp()
 
+with app.app_context():
+    db.create_all()  
+
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all() 
     app.run(debug=True, host="0.0.0.0", port=5000)
--- zadanie1/mainAsgi.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from asgiref.wsgi import WsgiToAsgi
-from app import createApp
-
-flaskApp = createApp()
-asgiApp = WsgiToAsgi(flaskApp)
