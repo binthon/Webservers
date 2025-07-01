787547cdd5011584eeaac29179eae06e164b8ad7
binthon <jakubmedyn@wp.pl>
2025-07-01 13:41:49 +0000

    add if

--- zadanie2/main.py
+++ zadanie2/main.py
@@ -1,10 +1,11 @@
 from app import createApp, db
 import app.model
-
+import os
 app = createApp()
 
 with app.app_context():
-    db.create_all()  
+    if not (os.path.exists("instance/sync.db") and os.path.exists("instance/async.db")):
+        db.create_all()  
 
 
 if __name__ == "__main__":
